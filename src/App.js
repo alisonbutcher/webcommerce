4145c07778753a83051109d383d02c012ef00902
Alison Butcher <alison@alisonbutcher.com>
2022-01-30 15:55:13 +1000

    update menu-item component to use react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Switch, Link, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <Link to='/'>Home</Link>
+    <h1>HATS PAGE</h1>
+  </div>
+
+
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
+
     </div>
   );
 }
